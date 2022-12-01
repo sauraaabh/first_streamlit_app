4f7a86d1a5f57b1639a7e93c27bc7ff801495081
sauraaabh <119560746+sauraaabh@users.noreply.github.com>
2022-12-01 12:51:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title(" My Parents New Healthy Diner")
-streamlit.title("Saurabh is a rockstar")
+streamlit.header(" Breakfast Menu")
+streamlit.text(" Poha ")
+streamlit.text(" Idli")
+streamlit.text(" Avacado on the toast")
+streamlit.text(" Egg omellette ")
