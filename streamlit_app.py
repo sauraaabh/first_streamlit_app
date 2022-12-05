1f75aa9256f577edaefb8e1a634901933a410b62
sauraaabh <119560746+sauraaabh@users.noreply.github.com>
2022-12-05 12:57:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,8 @@ try:
           fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
           fruityvice_normalized =pandas.json_normalize(fruityvice_response.json())  # flattens json i.e json_normalize()
           streamlit.dataframe(fruityvice_normalized)
- except URLError as e:
-     streamlit.error()
+except URLError as e:
+    streamlit.error()
 #streamlit.text(fruityvice_response.json())
 # below line flattens json i.e json_normalize()
 # shows dataframe as a table. 
