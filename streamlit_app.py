d86a710c5c13077737f8e40472f6afa9570d0539
sauraaabh <119560746+sauraaabh@users.noreply.github.com>
2022-12-05 12:22:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,10 @@ streamlit.dataframe(my_data_rows)
 
 # New section to display api response
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
 
+
+
+#this will not work but go with it now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
