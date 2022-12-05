24a1966c39e5d9f044113ddc85ed50e09ca250a6
sauraaabh <119560746+sauraaabh@users.noreply.github.com>
2022-12-05 12:56:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,22 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-# New section to display api response
+# New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+          streamlit.error("Please select a fruit to get information.")
+    else:
+          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+          fruityvice_normalized =pandas.json_normalize(fruityvice_response.json())  # flattens json i.e json_normalize()
+          streamlit.dataframe(fruityvice_normalized)
+ except URLError as e:
+     streamlit.error()
 #streamlit.text(fruityvice_response.json())
 # below line flattens json i.e json_normalize()
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # shows dataframe as a table. 
-streamlit.dataframe(fruityvice_normalized)
+
 
 #don't run anything past here
 streamlit.stop()
